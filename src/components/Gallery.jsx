5fa6a8aae05c9273853b0c7a6a2fd3ed94beda8d
Eduardo Juricic <edujuricic@gmail.com>
2025-06-30 11:55:58 -0300

    adicinando produtos na galeria

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 
-// IMPORTAÇÃO DAS IMAGENS DIRETAMENTE DO PASTA ASSETS
-// *** ATENÇÃO: Substitua os nomes dos arquivos e extensões (.jpg/.png) pelos seus arquivos reais! ***
+// IMPORTAÇÃO DAS IMAGENS ANTIGAS
 import hellokitty from "../assets/hello-kitty-caixas.jpg";
 import lembrancinhas from "../assets/lembrancinhas-aniversario.jpg";
 import batizado from "../assets/batizado.jpg";
@@ -11,7 +10,16 @@ import Kitfestavasco from "../assets/kit-festa.jpg";
 import topoboloanimal from "../assets/topo-animais.jpg";
 import Kitfestajunina from "../assets/kitfesta-junina.jpg";
 
+// --- IMPORTAÇÃO DAS NOVAS IMAGENS ---
+// ATENÇÃO: Os nomes dos arquivos devem corresponder aos que você salvou na pasta `src/assets`
+import lembrancinhasBiscuit from "../assets/Biscuit.jpg";
+import cadernoEucaristia from "../assets/PrimeiraEucaristia.jpg";
+import apliquesLeaozinho from "../assets/biscuitleaozinho.jpg";
+import agendaGirafa from "../assets/Agendaescolar.jpg";
+import manu from "../assets/cadernetavacinamanu.jpg";
+
 const products = [
+  // --- SEUS PRODUTOS ANTIGOS CONTINUAM AQUI ---
   {
     id: 1,
     category: "outros",
@@ -59,6 +67,37 @@ const products = [
     category: "kits",
     name: "Kit Festa",
     image: Kitfestajunina,
+  },
+  // --- NOVOS PRODUTOS ADICIONADOS ---
+  {
+    id: 9,
+    category: "outros",
+    name: "Lembrancinhas de Biscuit",
+    image: lembrancinhasBiscuit,
+  },
+  {
+    id: 10,
+    category: "cadernetas",
+    name: "Caderno Primeira Eucaristia",
+    image: cadernoEucaristia,
+  },
+  {
+    id: 11,
+    category: "outros",
+    name: "Apliques de Leãozinho em Biscuit",
+    image: apliquesLeaozinho,
+  },
+  {
+    id: 12,
+    category: "cadernetas",
+    name: "Agenda Escolar Personalizada",
+    image: agendaGirafa,
+  },
+  {
+    id: 13,
+    category: "cadernetas",
+    name: "Caderneta de  Vacinação",
+    image: manu,
   },
 ];
 
@@ -108,7 +147,6 @@ function Gallery() {
               key={product.id}
               className="bg-white rounded-lg shadow-md overflow-hidden transform transition duration-300 hover:scale-105 hover:shadow-xl hover:shadow-pink-200 flex flex-col"
             >
-              {/* CONTAINER DA IMAGEM: Aumentei a altura para h-80 */}
               <div className="relative w-full h-96 overflow-hidden rounded-t-lg">
                 <img
                   src={product.image}
@@ -116,7 +154,6 @@ function Gallery() {
                   className="absolute inset-0 w-full h-full object-cover"
                 />
               </div>
-              {/* FIM DO CONTAINER DA IMAGEM */}
 
               <div className="p-4 flex-grow">
                 <h3 className="text-xl font-semibold text-gray-900 mb-2">
